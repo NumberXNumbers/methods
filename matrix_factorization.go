ae8efe40baa72f5d060f7c7f93b62ba98b54071f
TRAVIS ALLEN SALAS COX <traviscox1990@utexas.edu>
2016-10-11 09:13:32 -0500

    added LDLT matrix factorization
    there is still work needed for this, such as determining if a matrix is
    positive-definite or hermitian. that work may need to go into the types
    project. and methods for calling/finding out will need to be added here
    so correct errors can be returned

--- matrix_factorization.go
+++ matrix_factorization.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	m "github.com/NumberXNumbers/types/gc/matrices"
+	v "github.com/NumberXNumbers/types/gc/vectors"
 	gcv "github.com/NumberXNumbers/types/gc/values"
 	gcvops "github.com/NumberXNumbers/types/gc/values/ops"
 )
@@ -63,4 +64,48 @@ func LU(A m.Matrix) (L, U, P m.Matrix, err error) {
 		}
 	}
 	return l, u, p, nil
+}
+
+// LDLt will return the L D mattrices of the L D Lt facorization of matrix A if it is
+// hermitian positive-definite else error
+// TODO: add in checks for positive-definite and hermitian forms
+func LDLt(A m.Matrix) (L, D  m.Matrix, err error) {
+	if !A.IsSquare() {
+		return nil, nil, errors.New("Matrix is not square")
+	}
+	degree, _ := A.Dim()
+	l := m.NewMatrix(degree, degree)
+	d := m.NewMatrix(degree, degree)
+
+	for i := 0; i < degree; i++ {
+		sumLi := gcv.NewValue()
+		diff := gcv.NewValue()
+		sumLj := gcv.NewValue()
+		counter := 0
+		tempVector := v.NewVector(v.RowSpace, i+1)
+		for j := 0; j < degree; j++ {
+			if j <= i - 1 {
+				tempValue := gcvops.Mult(l.Get(i, j), d.Get(j, j))
+				tempVector.Set(j, tempValue)
+				sumLi = gcvops.Add(sumLi, gcvops.Mult(l.Get(i, j), tempValue))
+			} else if j == i {
+				diff = gcvops.Sub(A.Get(i, i), sumLi)
+				if diff.Complex() == 0 {
+					return nil, nil, errors.New("Matrix can not be factorized")
+				}
+				d.Set(i, i, diff)
+				l.Set(i, i, 1)
+			} else {
+				if j > tempVector.Len() {
+					sumLi = gcv.NewValue()
+				} else {
+					sumLj = gcvops.Add(sumLj, gcvops.Mult(l.Get(j, counter), tempVector.Get(counter)))
+				}
+				div := gcvops.Div(gcvops.Sub(A.Get(j, i), sumLj), diff)
+				l.Set(j, i, div)
+				counter++
+			}
+		}
+	}
+	return l, d, nil
 }
--- matrix_factorization_test.go
+++ matrix_factorization_test.go
@@ -48,3 +48,40 @@ func TestLU(t *testing.T) {
 		t.Error("Expected Error")
 	}
 }
+
+func TestLDLtrans(t *testing.T) {
+	testVectorAa := v.MakeVector(v.RowSpace, 4, -1, 1)
+	testVectorAb := v.MakeVector(v.RowSpace, -1, 4.25, 2.75)
+	testVectorAc := v.MakeVector(v.RowSpace, 1, 2.75, 3.5)
+	testVectorsA := v.MakeVectors(v.RowSpace, testVectorAa, testVectorAb, testVectorAc)
+	testMatrixA := m.MakeMatrixAlt(testVectorsA)
+
+	L, D, errA := LDLt(testMatrixA)
+	if errA != nil {
+		t.Error("Unexpected Error")
+	}
+
+	A := mops.MustMultSimple(mops.MustMultSimple(L, D), m.MakeTransMatrix(L))
+	if !reflect.DeepEqual(testMatrixA, A) {
+		t.Errorf("Expected %+v, received %+v", testMatrixA, A)
+	}
+
+	testVectorsB := v.MakeVectors(v.RowSpace, testVectorAa, testVectorAb)
+	testMatrixB := m.MakeMatrixAlt(testVectorsB)
+
+	_, _, errB := LDLt(testMatrixB)
+	if errB == nil {
+		t.Error("Expected Error")
+	}
+
+	testVectorCa := v.MakeVector(v.RowSpace, 0, -1, 0)
+	testVectorCb := v.MakeVector(v.RowSpace, -1, 0, 2.75)
+	testVectorCc := v.MakeVector(v.RowSpace, 0, 2.75, 0)
+	testVectorsC := v.MakeVectors(v.RowSpace, testVectorCa, testVectorCb, testVectorCc)
+	testMatrixC := m.MakeMatrixAlt(testVectorsC)
+
+	_, _, errC := LDLt(testMatrixC)
+	if errC == nil {
+		t.Error("Expected Error")
+	}
+}
